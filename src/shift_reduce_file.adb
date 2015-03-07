ab5cacaa00664d2c28383ab1698d3959108ff08c
Stephane Carrez <Stephane.Carrez@gmail.com>
2015-03-07 18:42:38 +0100

    Add pragma Style_Checks (Off) in generated Ada files

--- src/shift_reduce_file.adb
+++ src/shift_reduce_file.adb
@@ -5,13 +5,14 @@ package body Shift_Reduce_File is
 
   -- SCCS_ID : constant String := "@(#) shift_reduce_file.ada, Version 1.2";
   -- Rcs_ID : constant String := "$Header: /dc/uc/self/arcadia/ayacc/src/RCS/shift_reduce_file.a,v 1.2 1993/05/31 22:36:35 self Exp self $";
-    
-    The_File : File_Type; 
+
+    The_File : File_Type;
 
     procedure Open_Write is
     begin
-        Create(The_File, Out_File, Get_Shift_Reduce_File_Name); 
-        Write_Line("package " & Shift_Reduce_Tables_Unit_Name & " is"); 
+        Create(The_File, Out_File, Get_Shift_Reduce_File_Name);
+        Write_Line("pragma Style_Checks (Off);");
+        Write_Line("package " & Shift_Reduce_Tables_Unit_Name & " is");
         Write_Line("");
         Write_Line("    type Small_Integer is range -32_000 .. 32_000;");
         Write_Line("");
@@ -21,7 +22,7 @@ package body Shift_Reduce_File is
         Write_Line("    end record;");
         Write_Line("    pragma Pack(Shift_Reduce_Entry);");
         Write_Line("");
-        Write_Line("    subtype Row is Integer range -1 .. Integer'Last;"); 
+        Write_Line("    subtype Row is Integer range -1 .. Integer'Last;");
         Write_Line("");
         Write_Line("  --pragma suppress(index_check);");
         Write_Line("");
@@ -30,8 +31,8 @@ package body Shift_Reduce_File is
         Write_Line("");
         Write_Line("    Shift_Reduce_Matrix : constant Shift_Reduce_Array :=");
         Write_Line("        ( (-1,-1) -- Dummy Entry");
-	New_Line(The_File);
-	exception
+    New_Line(The_File);
+    exception
              when Name_Error | Use_Error =>
                   Put_Line("Ayacc: Error Opening """ & Get_Shift_Reduce_File_Name & """.");
                   raise;
@@ -44,23 +45,23 @@ package body Shift_Reduce_File is
 
     procedure Close_Write is
     begin
-        Write_Line("end " & Shift_Reduce_Tables_Unit_Name & ";"); 
-	Close(The_File);
+        Write_Line("end " & Shift_Reduce_Tables_Unit_Name & ";");
+    Close(The_File);
     end Close_Write;
 
-    procedure Write(S: in String) is 
-    begin 
+    procedure Write(S: in String) is
+    begin
         Put(The_File, S) ;
     end;
 
     procedure Write_Line(S: in String) is
     begin
-	Put_Line(The_File, S); 
+    Put_Line(The_File, S);
     end Write_Line;
 
     procedure Write(C: in Character) is
     begin
-        Put(The_File,C);   
+        Put(The_File,C);
     end;
 
 end Shift_Reduce_File;
